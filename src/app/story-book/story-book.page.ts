51de42c14eddf0ea5badcd190063366f85b0862a
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-09-12 21:17:22 -0300

    Balões de texto mais bonitos

--- src/app/story-book/story-book.page.ts
+++ src/app/story-book/story-book.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Speech, Story } from '../models/Story';
 import { StoryService } from '../services/story.service';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-story-book',
@@ -10,25 +12,46 @@ import { StoryService } from '../services/story.service';
 export class StoryBookPage implements OnInit {
   public currentStory!: Story;
   public shownSpeechs: Speech[] = [];
-  public showHeader = true;
+  public maxSpeechs!: number;
   public storyHasStarted = false;
 
-  constructor(private storyService: StoryService) { }
+  @ViewChild(IonContent)
+  content!: IonContent;
+
+  constructor(private storyService: StoryService, private router: Router) {
+
+  }
 
   ngOnInit() {
     this.currentStory = this.storyService.getCurrentStory();
-    this.showHeader = false;
     this.storyHasStarted = false;
+    this.shownSpeechs = [];
+
+
+    this.storyHasStarted = true;
+    this.shownSpeechs.push(this.currentStory.speechs[0]);
+    this.shownSpeechs.push(this.currentStory.speechs[1]);
+    this.shownSpeechs.push(this.currentStory.speechs[2]);
+
+
+    this.maxSpeechs = this.currentStory.speechs.length;
     console.log(this.currentStory);
   }
 
   startStory() {
-    this.showHeader = true;
     this.storyHasStarted = true;
     this.shownSpeechs.push(this.currentStory.speechs[0]);
   }
 
   continueStory() {
     this.shownSpeechs.push(this.currentStory.speechs[this.shownSpeechs.length]);
+    this.content.scrollToBottom(500);
+  }
+
+  exit() {
+    this.storyHasStarted = false;
+    this.shownSpeechs = [];
+    this.maxSpeechs = this.currentStory.speechs.length;
+    this.router.navigateByUrl('/tabs/tab2');
   }
 }
